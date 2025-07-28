9e2bc0ca1486dbcf8e0d5c2f482be3a3fa0fe7d0
Magnaryuz <71626933+jqus@users.noreply.github.com>
2025-07-28 18:36:30 -0500

    Add files via upload

---
+++ starter_repo/app/head.js
@@ -0,0 +1,15 @@
+const Head = () => (
+<>
+<title>Charity Chain</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" href="/favicon.ico" />
+<meta name="description" content="Welcome to Charity Chain World, where we are dedicated to Transforming Lives through Blockchain. Our Decentralized Charity Initiatives leverage the power of blockchain technology to create a transparent and efficient platform for charitable giving. We take pride in our Innovative Blockchain Solutions for Social Impact, bridging the gap between generosity and technology. At Charity Chain World, we believe in harnessing the potential of blockchain to revolutionize charitable efforts, ensuring that every contribution makes a meaningful difference. Join us in building a future where technology and philanthropy come together to create lasting positive change." />
+<meta name="keywords" content="Transforming Lives through Blockchain, Decentralized Charity Initiatives, Innovative Blockchain Solutions for Social Impact, Bridging Generosity and Technology" />
+<meta name="robots" content="index, follow" />
+<meta name="language" content="en" />
+<meta name="author" content="Charity Chain World" />
+<meta name="date" content="2023-11-18" />
+</>
+);
+
+export default Head;
